c5c6b7af6d24999e3b826e86cc86c5db9a1f758c
SnpM <jpthek9@gmail.com>
2025-07-24 23:46:21 -0400

    Refactor project structure; update makefile

---
+++ c/include/executor.h
@@ -0,0 +1,5 @@
+#ifndef EXECUTOR_H
+#define EXECUTOR_H
+void execute(char command[]);
+
+#endif
---
+++ c/tests/main_test.c
@@ -0,0 +1,12 @@
+#define MUNIT_ENABLE_ASSERT_ALIASES
+#include "../munit/munit.h"
+#include "stdio.h"
+#include "../include/executor.h"
+
+void main(int argc, char** argv) {
+  test_executor()
+}
+
+void test_executor() {
+
+}

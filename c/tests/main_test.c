ebb97ed16ff3e4bb06337a53f89a49bdeaaa3ef9
SnpM <jpthek9@gmail.com>
2025-07-28 23:16:01 -0400

    munit works!

--- c/tests/main_test.c
+++ c/tests/main_test.c
@@ -3,11 +3,35 @@
 #include "stdio.h"
 #include "../include/executor.h"
 
-void test_executor() {
+//defined in test_executor.c
+extern MunitTest executor_tests[];
 
+
+static MunitSuite suites[] = {
+
+  //executor suite
+  {
+    "/executor", /* name */
+    executor_tests, /* tests */
+    NULL, /* suites */
+    1, /* iterations */
+    MUNIT_SUITE_OPTION_NONE /* options */
+  },
+
+  //EOF array
+  {NULL, NULL, NULL, 0, MUNIT_SUITE_OPTION_NONE}
+
+  };
+
+static MunitSuite main_suite = {
+  "/main", /* name */
+  NULL, /* tests */
+  suites, /* suites */
+  1, /* iterations */
+  MUNIT_SUITE_OPTION_NONE /* options */
+};
+
+int main(int argc, char **argv)
+{
+    return munit_suite_main(&main_suite, NULL, argc, argv);
 }
-
-void main(int argc, char** argv) {
-  test_executor();
-}
-
--- c/tests/test_executor.c
+++ c/tests/test_executor.c
@@ -1,2 +1,22 @@
+#include "executor.h"
+#include "munit.h"
 
-int whatever(){}
+
+static MunitResult test_execute(){
+    munit_assert_true(1);
+    return MUNIT_OK;
+}
+
+//https://nemequ.github.io/munit/#tests-and-suites
+MunitTest executor_tests[] = {
+  {
+    "/execute", /* name */
+    test_execute, /* test */
+    NULL, /* setup */
+    NULL, /* tear_down */
+    MUNIT_TEST_OPTION_NONE, /* options */
+    NULL /* parameters */
+  },
+  //end of array
+  { NULL, NULL, NULL, NULL, MUNIT_TEST_OPTION_NONE, NULL }
+};
